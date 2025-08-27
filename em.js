a072247ba46782cd3568b4d0effdc409b231c26c
unknown <mrcplabon@gmail.com>
2025-08-28 00:26:55 +0600

    Call me

--- em.js
+++ em.js
@@ -19,3 +19,23 @@ const h6 = count('heart6');
 const h7 = count('heart7');
 const h8 = count('heart8');
 const h9 = count('heart9');
+
+//Common function for calling..
+function call(callId, titleId, numId){
+    document.getElementById(callId).addEventListener('click', function(event){
+        event.preventDefault();
+        const titleN = document.getElementById(titleId).innerText;
+        const numN = document.getElementById(numId).innerText;
+        alert("Calling " + titleN + '  ' + numN + "...")
+    })
+}
+
+const do1 = call('call', 'title', 'num');
+const do2 = call('call2', 'title2', 'num2');
+const do3 = call('call3', 'title3', 'num3');
+const do4 = call('call4', 'title4', 'num4');
+const do5 = call('call5', 'title5', 'num5');
+const do6 = call('call6', 'title6', 'num6');
+const do7 = call('call7', 'title7', 'num7');
+const do8 = call('call8', 'title8', 'num8');
+const do9 = call('call9', 'title9', 'num9');
